6e8fd45eb1e86aa41ff84baee1f9b9f804d2cd7d
Ivan Iarunichev <ivani94@educastur.es>
2026-03-22 11:25:58 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'traiviu',
+  webDir: 'www'
+};
+
+export default config;
