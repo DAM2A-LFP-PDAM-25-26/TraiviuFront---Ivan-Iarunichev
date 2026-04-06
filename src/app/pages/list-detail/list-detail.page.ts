19c152cdac257a0758733d85e30bafc427931dec
Ivan Iarunichev <ivani94@educastur.es>
2026-04-06 22:15:03 +0200

    Update List Item

--- src/app/pages/list-detail/list-detail.page.ts
+++ src/app/pages/list-detail/list-detail.page.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ListItemService } from '../../services/list-item';
 import { ListsService, TraiviuList } from '../../core/api/lists';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-detail',
   templateUrl: './list-detail.page.html',
   styleUrls: ['./list-detail.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, RouterModule]
+  imports: [IonicModule, CommonModule, RouterModule, FormsModule],
 })
 export class ListDetailPage implements OnInit {
-
   listId: string = '';
+  filtroPeriodo: string = 'mes';
   nombreLista: string = 'Cargando...';
   peliculas: any[] = [];
 
@@ -24,8 +25,9 @@ export class ListDetailPage implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private listItemService: ListItemService,
-    private listsService: ListsService
-  ) { }
+    private listsService: ListsService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.listId = this.route.snapshot.paramMap.get('id') || '';
@@ -39,7 +41,7 @@ export class ListDetailPage implements OnInit {
   cargarDatosDeLaLista() {
     this.listsService.getListsByUser(this.testUserId).subscribe({
       next: (listas: TraiviuList[]) => {
-        const listaActual = listas.find(lista => lista.id === this.listId);
+        const listaActual = listas.find((lista) => lista.id === this.listId);
 
         if (listaActual) {
           this.nombreLista = listaActual.name;
@@ -50,7 +52,7 @@ export class ListDetailPage implements OnInit {
       error: (err: any) => {
         console.error('Error al cargar el nombre de la lista:', err);
         this.nombreLista = 'Detalle de Lista';
-      }
+      },
     });
   }
 
@@ -62,15 +64,19 @@ export class ListDetailPage implements OnInit {
       },
       error: (err) => {
         console.error('Error al cargar la lista', err);
-      }
+      },
     });
   }
 
   borrarPelicula(itemId: string) {
     this.listItemService.removeMovieFromList(itemId).subscribe({
       next: () => {
-        this.peliculas = this.peliculas.filter(p => p.id !== itemId);
-      }
+        this.peliculas = this.peliculas.filter((p) => p.id !== itemId);
+      },
     });
+  }
+
+  goBack() {
+    this.router.navigate(['/tabs/lists']);
   }
 }
